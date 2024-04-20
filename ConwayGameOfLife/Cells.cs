feb8b1c2ed158d590b04facc43779145dad0b01e
Rilr20 <nrrl99@gmail.com>
2024-04-20 23:45:19 +0200

    removes and add cells

--- ConwayGameOfLife/Cells.cs
+++ ConwayGameOfLife/Cells.cs
@@ -9,6 +9,7 @@ namespace ConwayGameOfLife
     internal class Cells
     {
 
+
         private List<int[]> cellList = new List<int[]>();
 
         public void Cell(int x, int y, bool is_alive)
@@ -43,6 +44,31 @@ namespace ConwayGameOfLife
             }
 
             return surroundingCells;
+        }
+        public void cellStatus(int[] cell)
+        {
+            /**
+             * Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+             * Any live cell with two or three live neighbors lives on to the next generation.
+             * Any live cell with more than three live neighbors dies, as if by overpopulation.
+             * Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+             **/
+            List<int[]> neighbourCells = findSurroundingCells(cell[0], cell[1]);
+
+            if (neighbourCells.Count < 2 || neighbourCells.Count > 3)
+            {
+                //remove this cell from list
+                cellList.Remove(cell);
+            }
+        }
+        public void newCells(int x, int y)
+        {
+            List<int[]> neighbourCells = findSurroundingCells(x, y);
+            if (neighbourCells.Count == 3)
+            {
+                int[] newCell = { x, y, 1 };
+                cellList.Add(newCell);
+            }
         }
     }
 }
